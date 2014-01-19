83d6734050eb5efcf136143ee533ca32fb931173
Colin Kinloch <colinmkinloch@gmail.com>
2014-01-19 00:25:28 +0000

    starting portmidi

--- src/MuzakTest.cc
+++ src/MuzakTest.cc
@@ -4,12 +4,18 @@
 #include <portaudio.h>
 #include <portmidi.h>
 
-
+int i=0;
 int execute;
 void trap(int signal){ execute = 0; }
 
-PmStream* midi;
+PmStream* midiStream;
+PmTimeProcPtr midiTime;
+PmTimeProcPtr midiTimestamp;
 PmEvent buffer[1];
+
+void midiRead()
+{
+}
 
 int main()
 {
@@ -18,17 +24,29 @@ int main()
 	int err;
 	err = Pm_Initialize();
 	if(err != pmNoError)
-	 printf("PortAudio error: %s\n", Pm_GetErrorText((PmError)err));
+	 printf("PortMidi error: %s\n", Pm_GetErrorText((PmError)err));
 	err = Pa_Initialize();
 	if(err != paNoError)
 	 printf("PortAudio error: %s\n", Pa_GetErrorText((PaError)err));
+	
+	const PmDeviceInfo* midiDevice = Pm_GetDeviceInfo(i);
+	printf("Input: %s\n", midiDevice->name);
+	Pm_OpenInput(&midiStream, i, NULL, 128, NULL, NULL);
 	
 	signal(SIGINT, &trap);
 	execute = 1;
 	while(execute)
 	{
+		midiRead();
 	}
-	signal(SIGINT, SIG_DFL);
 	printf("Shutting down!\n");
+	signal(SIGINT, SIG_DFL);
+	
+	Pm_Close(midiStream);
+	
+	Pa_Terminate();
+	Pm_Terminate();
+	
+	
 	return 0;
 }
